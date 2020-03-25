e0b4c4586b597c4ecc7850c255a8969f44b147a3
eloc0n <johnisb.21@gmail.com>
2020-03-25 15:46:01 +0200

    created signals.py for stripe api

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class UserStripe(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    stripe_id = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.stripe_id
+
